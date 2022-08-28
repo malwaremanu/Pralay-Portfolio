0d63131f7f57d9a753b19006f3146d9749bd8f9c
Manupal Choudhary <tech.manujpr@gmail.com>
2022-08-28 17:27:46 +0530

    AES256 Implemented

--- pages/cry.vue
+++ pages/cry.vue
@@ -1,36 +1,41 @@
 <template>
   <div class="p-6">
     <div class="bg-green-50 rounded-md p-2">
-      <div class="p-2">Text Input to Encrypt </div>
+      <div class="p-2">Text Input to Encrypt</div>
       <div class="p-2">
-        <input type="text" v-model="txt" class="bg-green-200"/>
+        <input type="text" v-model="ntxt.data.user" class="bg-green-200" />
+        <input type="text" v-model="ntxt.data.pass" class="bg-green-200" />
       </div>
-      <div class="px-3">
-        Key : {{ secret }}
-      </div>
+      <div class="px-3">Key : {{ secret }}</div>
       <div class="p-2">
-        <button @click="enc" class="bg-green-400 hover:bg-green-500 text-white">Submit</button>
+        <button
+          @click="enc(ntxt.data)"
+          class="bg-green-400 hover:bg-green-500 text-white"
+        >
+          Submit
+        </button>
       </div>
       {{ crpto }}
     </div>
 
-    <hr />    
+    <hr />
 
     <div class="bg-green-50 rounded-md p-2">
-      <div class="p-2">Text Input to Decrypt </div>
+      <div class="p-2">Text Input to Decrypt</div>
       <div class="p-2">
-        <input type="text" v-model="txt" class="bg-green-200"/>
+        <input type="text" v-model="txt.data" class="bg-green-200 w-full" />
       </div>
-      <div class="px-3">
-        Key : {{ secret }}
-      </div>
+      <div class="px-3">Key : {{ secret }}</div>
       <div class="p-2">
-        <button @click="dec" class="bg-green-400 hover:bg-green-500 text-white">Submit</button>
+        <button
+          @click="dec(txt.data)"
+          class="bg-green-400 hover:bg-green-500 text-white"
+        >
+          Submit
+        </button>
       </div>
       {{ orig }}
     </div>
-
-
   </div>
 </template>
 <script>
@@ -39,27 +44,42 @@ var CryptoJS = require('crypto-js')
 export default {
   data() {
     return {
-      txt: "this is something",
-      secret: "v8y/B?E(H+MbQeShVmYq3t6w9z$C&F)J",
-      crpto : '',
-      orig : ''
+      ntxt: {
+        data: {
+          user : "manu",
+          pass : "password"
+        },
+      },
+      txt: {
+        data: 'g4Wc4MXN8qB1PPbngFWQav3Vp9+Pfj0EiuA0s4S8MTnkoiTkXgWiTwpGUSmL0Pfa',
+      },
+      //secret: "dRgUkXp2s5v8y/B?",
+      secret: CryptoJS.enc.Utf8.parse("PoncU&*()/12345aPoncU&*()/12345a"),
+      crpto: '',
+      orig: '',
+      
     }
   },
   methods: {
-    enc() {
-      // Encrypt
-      var ciphertext = CryptoJS.AES.encrypt(this.txt, this.secret).toString()
-      console.log('Cipher Text', ciphertext)
-      this.crpto = ciphertext
-      console.log('Decoded Text', ciphertext) // 'my message'
+    enc(x) {
+      // Encrypt      
+      var encrypted = CryptoJS.AES.encrypt(JSON.stringify(x), this.secret, {
+        mode: CryptoJS.mode.ECB,
+      })
+      encrypted = encrypted.toString()
+      console.log(encrypted)
+      this.txt.data = encrypted
+      return encrypted
     },
 
-    dec() {      
+    dec(x) {
       // Decrypt
-      var bytes = CryptoJS.AES.decrypt(this.crpto, this.secret)
-      var originalText = bytes.toString(CryptoJS.enc.Utf8)
-      this.orig = originalText
-      console.log('Decoded Text', originalText) // 'my message'
+      var decrypted = CryptoJS.AES.decrypt(x, this.secret, {
+        mode: CryptoJS.mode.ECB,
+      })
+      decrypted = decrypted.toString(CryptoJS.enc.Utf8)      
+      this.orig = decrypted
+      return decrypted
     },
   },
 }
